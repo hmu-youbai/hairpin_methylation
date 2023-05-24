2600c4b236efe3bd3df5a5c1c7043f1fb54bd334
youbai <78633949+hmu-youbai@users.noreply.github.com>
2023-05-24 18:34:46 +0800

    Update hmc_extractor.py

--- hairpin/hmc_extractor.py
+++ hairpin/hmc_extractor.py
@@ -323,7 +323,7 @@ def main():
             ccc_CHH = ccc_CHH + int(line[2])
             ccc_CN = ccc_CN + int(line[3])
 
-    from log import calculate_statistics
+    from hairpin.log import calculate_statistics
     calculate_statistics(args.output,ccc_CpG,ccc_CHG,ccc_CHH,"hmc",args)
 
 
